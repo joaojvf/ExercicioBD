c9ffadfac7495ace28a86c0cc4d4154092cc481d
Matheus Ramos Giacomini <matheus.dua@hotmail.com>
2018-09-24 08:37:13 -0300

    inicializando projeto

---
+++ ExercicioBD/vwCliente.aspx.cs
@@ -0,0 +1,144 @@
+﻿using ExemploBD.Models;
+using ExemploBD.Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExemploBD
+{
+    public partial class vwCliente : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnAdd_Click(object sender, EventArgs e)
+        {
+            ClienteDAO clienteDao = new ClienteDAO();
+
+            string nome = TxtNome.Text;
+            string cpf = TxtCpf.Text;
+            string rg = TxtRg.Text;
+            string id = TxtId.Text;
+            
+            if((nome.Equals("") || nome.Length > 45 || cpf.Equals("") || cpf.Length > 11 || rg.Equals("") || rg.Length > 15)) 
+            {
+                LblResultado.Text = "Dados estão inseridos de forma incorreta!!";
+            }else if(id != "")
+            {
+                BtnAlterar_Click(sender, e);
+            }
+            else
+            {
+                Cliente cliente = new Cliente()
+                {
+                    Nome = nome,
+                    Cpf = cpf,
+                    Rg = rg,
+                };
+
+                cliente = clienteDao.Inserir(cliente);
+                if (cliente != null)
+                {
+                    Session["cliente"] = cliente;
+                    Response.Redirect("~/vwEndereco.aspx");
+                }
+            }
+            
+        }
+
+        protected void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            ClienteDAO clienteDao = new ClienteDAO();
+
+            int id = 0;
+            if (int.TryParse(TxtId.Text, out id) && id > 0)
+            {
+                Cliente c = clienteDao.BuscarPorId(id);
+                LblResultado.Text = "";
+                if (c != null)
+                {
+                    TxtNome.Text = c.Nome;
+                    TxtCpf.Text = c.Cpf;
+                    TxtRg.Text = c.Rg;
+                }
+                else
+                {
+                    TxtNome.Text = "";
+                    TxtCpf.Text = "";
+                    TxtRg.Text = "";
+                }
+            }
+
+            
+        }
+
+        protected void BtnAlterar_Click(object sender, EventArgs e)
+        {
+            ClienteDAO clienteDao = new ClienteDAO();
+
+            int id = 0;
+            string nome = TxtNome.Text;
+            string cpf = TxtCpf.Text;
+            string rg = TxtRg.Text;
+
+            if (!int.TryParse(TxtId.Text, out id) || id < 0)
+            {
+                LblResultado.Text = "Insira o Id";
+            }
+            else if (nome.Equals("") || nome.Length > 45 || cpf.Equals("") || cpf.Length > 45 || rg.Equals("") || rg.Length > 9 )
+                
+            {
+                LblResultado.Text = "Dados estão inseridos de forma incorreta!!";
+            }
+            else
+            {
+                Cliente c = clienteDao.Alterar(new Cliente
+                {
+                    Id = id,
+                    Nome = nome,
+                    Cpf = cpf,
+                    Rg = rg
+                });
+
+                if (c != null)
+                {
+                    LblResultado.Text = "Cliente Alterado com Sucesso";
+                    Session["cliente"] = c;
+                    Response.Redirect("~/vwEndereco.aspx");
+                }
+            }
+        }
+
+        protected void BtnDel_Click(object sender, EventArgs e)
+        {
+            ClienteDAO clienteDao = new ClienteDAO();
+            Cliente c = new Cliente
+            {
+                Id = Convert.ToInt32(TxtId.Text)
+            };
+
+            if (clienteDao.validaDeletar(c))
+            {
+                if (clienteDao.Deletar(c))
+                {
+                    LblResultado.Text = "Cliente Deletado com Sucesso";
+                }
+            }
+            else
+            {   
+
+                LblResultado.Text = "Cliente tem enderecos cadastrados!";
+            }
+        }
+
+        protected void BtnListar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ExercicioBD/vwCliente.aspx.designer.cs
@@ -0,0 +1,150 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ExemploBD {
+    
+    
+    public partial class vwCliente {
+        
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle LblId.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblId;
+        
+        /// <summary>
+        /// Controle TxtId.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtId;
+        
+        /// <summary>
+        /// Controle LblNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblNome;
+        
+        /// <summary>
+        /// Controle TxtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtNome;
+        
+        /// <summary>
+        /// Controle LblCpf.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblCpf;
+        
+        /// <summary>
+        /// Controle TxtCpf.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtCpf;
+        
+        /// <summary>
+        /// Controle LblRg.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblRg;
+        
+        /// <summary>
+        /// Controle TxtRg.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtRg;
+        
+        /// <summary>
+        /// Controle BtnAdd.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnAdd;
+        
+        /// <summary>
+        /// Controle BtnAlterar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnAlterar;
+        
+        /// <summary>
+        /// Controle BtnDel.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnDel;
+        
+        /// <summary>
+        /// Controle BtnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnBuscar;
+        
+        /// <summary>
+        /// Controle BtnListar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnListar;
+        
+        /// <summary>
+        /// Controle LblResultado.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblResultado;
+    }
+}
